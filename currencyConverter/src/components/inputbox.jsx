137bfead5b7431a3fef910de3cb59814a3e08cde
Pranav8722 <pranavbadaskar06@gmail.com>
2025-08-14 17:28:38 +0530

    created hook  in  currency converter app

---
+++ currencyConverter/src/components/inputbox.jsx
@@ -0,0 +1,45 @@
+function InputBox1({
+  dest,
+  selectCurrency,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  isDisabled = false
+}) {
+  return (
+    <div className="bg-gray-400 rounded-xl p-2  my-4">
+      <div>
+        <h1 className='m-2 font-semibold'>{dest}</h1>
+      </div>
+
+      <div className='flex'>
+        <input
+          className='h-[60px] w-[470px] ml-4 mb-12 rounded-l-xl px-3'
+          type="number"
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+          disabled={isDisabled}
+        />
+
+        <select
+          className='bg-black text-white rounded-r-xl h-[60px] px-2'
+          value={selectCurrency}
+          onChange={(e) =>
+            onCurrencyChange && onCurrencyChange(e.target.value)
+          }
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default InputBox1
---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+
+import {  useEffect, useState } from 'react'
+function currencyInfo(currency){
+    const [data,setData]=useState({})
+useEffect(()=>{
+     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+    
+    .then(res=>res.json())
+    .then((res)=>setData(res[currency]))
+    .catch((err)=>console.log(err))
+
+
+  },[currency])
+
+return data
+
+}
+export default currencyInfo
